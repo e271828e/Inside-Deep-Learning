92537e122fc6356c770f7825f47cd9781fbad573
Miguel Alonso <miguel883@gmail.com>
2024-02-29 22:25:08 +0100

    Finished chapter 2 exercises 3 and 4

--- tools.py
+++ tools.py
@@ -1,3 +1,4 @@
+import os
 import time
 import functools
 
@@ -85,13 +86,22 @@ def eval_network(model, loaders, metrics):
 
 
 def train_network( model, loss_function, optimizer, train_loader, eval_loaders=None, eval_metrics=None,
-                  final_epoch=1, save_checkpoint=None, lr_schedule=None, show_progress=True):
+                  epochs=4, save_path=None, save_id=None, save_interval=2, load_file=None, show_progress=True):
 
     model_training = model.training
 
-    #GET THESE FROM CHECKPOINT
-    initial_epoch = 0 #number of completed training epochs before we start
-    initial_time = 0
+    if load_file is not None:
+        checkpoint = torch.load(load_file, map_location=next(model.parameters()).device)
+        model.load_state_dict(checkpoint['model_state'])
+        optimizer.load_state_dict(checkpoint['optimizer_state'])
+        last_epoch = checkpoint["epoch"]
+        last_time = checkpoint["time"]
+    else:
+        last_epoch = 0
+        last_time = 0
+
+    if save_id is None:
+        save_id = "checkpoint"
 
     if eval_loaders is None:
         eval_loaders = {}
@@ -99,30 +109,32 @@ def train_network( model, loss_function, optimizer, train_loader, eval_loaders=N
     if eval_metrics is None:
         eval_metrics = {"Loss": loss_function}
 
-    step_results = {"Epochs Trained": [initial_epoch], "Accumulated Time": [initial_time]}
+    step_results = {"Last Epoch": [last_epoch], "Last Time": [last_time]}
 
     #get current model evaluation before starting training
     scores = eval_network(model, eval_loaders, eval_metrics)
     for name, score in scores.items():
         step_results[name] = [score]
 
-    for epoch in tqdm(range(initial_epoch + 1, final_epoch + 1), desc="Epoch", disable=(not show_progress)):
+    for epoch in tqdm(range(last_epoch + 1, last_epoch + epochs + 1), desc="Epoch", disable=(not show_progress)):
 
         model.train()
         train_results = run_epoch( model, loss_function, optimizer, train_loader, desc=None, show_progress=show_progress)
-        step_results["Epochs Trained"].append(epoch)
-
-        #add epoch training time to the last accumulated value
-        step_results["Accumulated Time"].append(step_results["Accumulated Time"][-1] + train_results["time"])
+        step_results["Last Epoch"].append(epoch)
+        step_results["Last Time"].append(train_results["time"])
 
         scores = eval_network(model, eval_loaders, eval_metrics)
         for name, score in scores.items():
             step_results[name].append(score)
 
 
+        if save_path is not None and epoch % save_interval == 0:
+            file_name = save_id + "_epoch" + str(epoch) + ".pt"
+            torch.save( {"epoch": epoch, "time": train_results["time"],
+                         "model_state": model.state_dict(), "optimizer_state": optimizer.state_dict()},
+                         os.path.join(save_path, file_name))
+
     #restore model to its original mode
     model.train(model_training)
 
-    #save checkpoints on every epoch that is a multiple of checkpoint_interval or the last one
-    
     return pd.DataFrame.from_dict(step_results)
