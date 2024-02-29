38ab741b697ed31a9f2a72050e92890315cb536a
Miguel Alonso <miguel883@gmail.com>
2024-02-29 01:00:54 +0100

    Implemented main network evaluation and training functions, checkpoint functionality is next

--- tools.py
+++ tools.py
@@ -22,16 +22,16 @@ def from_logits(f_metric):
 
 
 
-def run_epoch(model, loss_function, optimizer, data_loader, metrics=None, desc=None, show_progress=True):
+def run_epoch(model, loss_function, optimizer, data_loader, desc=None, show_progress=True):
 
     #the computing device is found from the first model parameter. we assume all
     #parameters in the model are stored in the same device, otherwise we will
     #get an error later on
     computing_device = next(model.parameters()).device
 
-    batch_losses = []
     y_batches = []
     y_hat_batches = []
+    torch.cuda.synchronize()
     t_start = time.time()
     for x_batch, y_batch in tqdm( data_loader, desc=desc, leave=False, disable=(not show_progress)):
         
@@ -40,18 +40,16 @@ def run_epoch(model, loss_function, optimizer, data_loader, metrics=None, desc=N
 
         y_hat_batch = model(x_batch)
 
-        batch_loss = loss_function(y_hat_batch, y_batch)
-
         if model.training:
+            batch_loss = loss_function(y_hat_batch, y_batch)
             batch_loss.backward()
             optimizer.step()
             optimizer.zero_grad()
 
-        batch_losses.append(batch_loss.item())
-
         y_batches.append(y_batch.detach())
         y_hat_batches.append(y_hat_batch.detach()) 
 
+    torch.cuda.synchronize()
     t_end = time.time()
 
     y_epoch = torch.cat(y_batches) #along first dimension
@@ -59,13 +57,72 @@ def run_epoch(model, loss_function, optimizer, data_loader, metrics=None, desc=N
 
     results = {}
     results["time"] = t_end - t_start
-    results["loss"] = np.mean(batch_losses)
-
-    if metrics is not None:
-        for name, func in metrics.items():
-            try:
-                results[name] = func(y_hat_epoch, y_epoch)
-            except:
-                results[name] = float("NaN")
+    results["y_hat"] = y_hat_epoch
+    results["y"] = y_epoch
 
     return results
+
+
+def eval_network(model, loaders, metrics):
+
+    model_training = model.training
+    model.eval()
+
+    scores = {}
+    for loader_name, loader in loaders.items():
+        results = run_epoch( model, None, None, loader, desc=None, show_progress=False)
+        for metric_name, metric in metrics.items():
+            key = loader_name + " " + metric_name
+            try:
+                scores[key] = metric(results["y_hat"], results["y"]).item()
+            except:
+                scores[key] = float("NaN")
+
+    #restore model to its previous mode
+    model.train(model_training)
+
+    return scores
+
+
+def train_network( model, loss_function, optimizer, train_loader, eval_loaders=None, eval_metrics=None,
+                  final_epoch=1, save_checkpoint=None, lr_schedule=None, show_progress=True):
+
+    model_training = model.training
+
+    #GET THESE FROM CHECKPOINT
+    initial_epoch = 0 #number of completed training epochs before we start
+    initial_time = 0
+
+    if eval_loaders is None:
+        eval_loaders = {}
+
+    if eval_metrics is None:
+        eval_metrics = {"Loss": loss_function}
+
+    step_results = {"Epochs Trained": [initial_epoch], "Accumulated Time": [initial_time]}
+
+    #get current model evaluation before starting training
+    scores = eval_network(model, eval_loaders, eval_metrics)
+    for name, score in scores.items():
+        step_results[name] = [score]
+
+    for epoch in tqdm(range(initial_epoch + 1, final_epoch + 1), desc="Epoch", disable=(not show_progress)):
+
+        model.train()
+        train_results = run_epoch( model, loss_function, optimizer, train_loader, desc=None, show_progress=show_progress)
+        step_results["Epochs Trained"].append(epoch)
+
+        #add epoch training time to the last accumulated value
+        step_results["Accumulated Time"].append(step_results["Accumulated Time"][-1] + train_results["time"])
+
+        scores = eval_network(model, eval_loaders, eval_metrics)
+        for name, score in scores.items():
+            step_results[name].append(score)
+
+
+    #restore model to its original mode
+    model.train(model_training)
+
+    #save checkpoints on every epoch that is a multiple of checkpoint_interval or the last one
+    
+    return pd.DataFrame.from_dict(step_results)
